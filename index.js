0e4725192e6355523ac4f82b97a5ea32c12f1ade
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-05-12 17:18:15 -0300

    Confirmando as alterações

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import { database } from "./src/db.js";
 import { veiculosRepository } from "./src/database/veiculos.js";
-import { usersRepository } from "./src/database/users.js";
+
 import express from "express";
+
 import path from "path";
 import { fileURLToPath } from "url";
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const db = await database.connect(); // conecta ao banco de dados
-const server = express();
 
+const db = await database.connect(); // conecta ao banco de dados
+
+const server = express();
 server.use(express.urlencoded({ extended: true })); //habilita o uso do post dentro das rotas
 server.use(express.static(path.join(__dirname + "/public"))); //habilita o uso de arquivos estaticos
+
 server.set("views", path.join(__dirname + "/src/views")); //define a pasta de views
 server.set("view engine", "ejs"); //define o motor de views
 
@@ -30,36 +31,16 @@ server.get("/singup", (req, res) => {
 });
 
 server.post("/singup-cadastrar", (req, res) => {
-
-	let userData = req.body;
-
-	// Debbugs para entender o body
-	// console.log(userData.password[0]);
-	// console.log(userData);
-
-
-	if (userData.password[0] == userData.password[1]){
-		let users = usersRepository.setUser(
-			// Dados em formato JSON para cadastro no banco
-			{
-				"name" : userData.name,
-				"date" : userData.nascimento,
-				"telefone" : userData.telefone,
-				"genero" : userData.genero,
-				"email": userData.email,
-				"password" : userData.password[0] // Password em primeira posicao
-			}
-		);
-		if (users){
-			res.render("singin", {}); // Caso users seja true redireciona para "singin"
-		}
-	} else {
-		res.render("singup", {}); // Caso password errado retorna para "singup"
-	}
+	console.log(req.body);
 });
 
 server.get("/admin", (req, res) => {
 	res.render("admin", {});
+});
+
+server.get("/admloja", async (req, res) => {
+	let veiculos = await veiculosRepository.getVeiculos();
+	res.render("admLoja", { veiculos });
 });
 
 server.listen(3000, () => {
