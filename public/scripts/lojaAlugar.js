5af4047aa136a67d80fd3ba0a4a1c1dd2a1608c8
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-05-15 15:34:19 -0300

    Tela de alugar carro finalizada

--- public/scripts/lojaAlugar.js
+++ public/scripts/lojaAlugar.js
@@ -26,8 +26,36 @@ valor.style.display = "none";
 pagamentos.style.display = "none";
 
 datafim.addEventListener("change", () => {
-	valor.style.display = "block";
+	valor.style.display = "flex";
 	pagamentos.style.display = "block";
+
+	let data1 = dataini.value.split("-");
+	let data2 = datafim.value.split("-");
+	let mes = [];
+	let ano1 = parseInt(data1[0]);
+	let ano2 = parseInt(data2[0]);
+	let mes1 = parseInt(data1[1]);
+	let mes2 = parseInt(data2[1]);
+	let dia1 = parseInt(data1[2]);
+	let dia2 = parseInt(data2[2]);
+	if (ano1 % 4 == 0 && (ano1 % 100 != 0 || ano1 % 400 == 0)) {
+		ano1 *= 366;
+		mes = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+		mes1 = mes[mes1 - 1];
+	} else if (ano2 % 4 == 0 && (ano2 % 100 != 0 || ano2 % 400 == 0)) {
+		ano2 *= 366;
+		mes = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+		mes2 = mes[mes2 - 1];
+	} else {
+		ano1 = parseInt(data1[0] * 365);
+		ano2 = parseInt(data2[0] * 365);
+		mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+		mes1 = mes[mes1 - 1];
+		mes2 = mes[mes2 - 1];
+	}
+	let diferenca = ano2 + mes2 + dia2 - ano1 - mes1 - dia1 + 1;
+	let diaria = parseFloat(document.querySelector("#diaria").value);
+	document.querySelector("#valor").value = diferenca * diaria;
 });
 
 const pagamento = () => {
@@ -43,9 +71,13 @@ const pagamento = () => {
 		forma[1].classList.add("selecionado");
 		forma[0].classList.remove("selecionado");
 		forma[2].classList.remove("selecionado");
-	} else {
+	} else if (cartao.checked) {
 		forma[0].classList.remove("selecionado");
 		forma[1].classList.remove("selecionado");
 		forma[2].classList.add("selecionado");
+	} else {
+		forma[0].classList.remove("selecionado");
+		forma[1].classList.remove("selecionado");
+		forma[2].classList.remove("selecionado");
 	}
 };
